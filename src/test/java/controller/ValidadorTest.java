90dea8dec3b1002af9db0c3b05c410db563dadd7
Christian Seidler <ch.cseidler@gmail.com>
2022-02-20 13:52:37 -0300

    inicio da implementação dos testes unitarios

--- src/test/java/controller/ValidadorTest.java
+++ src/test/java/controller/ValidadorTest.java
@@ -1,32 +1,66 @@
 package test.java.controller;
 
+import main.java.controller.Validador;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ValidadorTest {
 
     @Test
-    void validaNome() {
-        assertAll(
+    void deveRetornarTrueNaValidacaoDoNome() {
+       assertAll( () -> assertTrue(Validador.validaNome("Andressa"))
+               ,() -> assertTrue(Validador.validaNome("De"))
+               ,() -> assertTrue(Validador.validaNome("Christian Seidler"))
+               ,() -> assertTrue(Validador.validaNome("Nome Completo"))
+               );
 
+    }
+
+    @Test
+    void deveRetornarFalseNaValidacaoDoNome() {
+        assertAll( () -> assertFalse(Validador.validaNome("A"))
+                ,() -> assertFalse(Validador.validaNome("Chr1stian"))
+                ,() -> assertFalse(Validador.validaNome("Christian_Seidler"))
+                ,() -> assertFalse(Validador.validaNome("Nome&Completo"))
         );
 
     }
 
     @Test
-    void validaTelefone() {
+    void deveRetornarTrueNaValidacaoDoTelefone() {
+        assertAll( () -> assertTrue(Validador.validaNome("Andressa"))
+                ,() -> assertTrue(Validador.validaNome("De"))
+                ,() -> assertTrue(Validador.validaNome("Christian Seidler"))
+                ,() -> assertTrue(Validador.validaNome("Nome Completo"))
+        );
+
     }
 
     @Test
-    void validaData() {
+    void deveRetornarFalseNaValidacaoDoTelefone() {
+        assertAll( () -> assertFalse(Validador.validaNome("A"))
+                ,() -> assertFalse(Validador.validaNome("Chr1stian"))
+                ,() -> assertFalse(Validador.validaNome("Christian_Seidler"))
+                ,() -> assertFalse(Validador.validaNome("Nome&Completo"))
+        );
+
     }
 
-    @Test
-    void validaNota() {
-    }
-
-    @Test
-    void validaID() {
-    }
+//    @Test
+//    void validaTelefone() {
+//    }
+//
+//    @Test
+//    void validaData() {
+//    }
+//
+//    @Test
+//    void validaNota() {
+//    }
+//
+//    @Test
+//    void validaID() {
+//    }
 }
