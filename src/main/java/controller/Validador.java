f3618aa7b6850c51a74e5719d683bcf3de466a83
Christian Seidler <ch.cseidler@gmail.com>
2022-02-20 14:07:43 -0300

    refatoração no nome das variaveis

--- src/main/java/controller/Validador.java
+++ src/main/java/controller/Validador.java
@@ -7,12 +7,12 @@ public class Validador {
 
     /**
      * Valida via regex se a String recebida contem apenas letras, nao aceitando numeros e caracteres especiais.
-     * @param s parametro de entrada enviado pelo usuario.
+     * @param nome parametro de entrada enviado pelo usuario.
      * @return Este metodo retorna verdadeira se tiver mais que 1 caracter e for apenas letras;
      *                     retorna falso se receber caracteres nao aceitos.
      */
-    public static boolean validaNome(String s) {
-        if (s.matches("^[A-Za-zÀ-ÿ \"'-]+$") && s.length() > 1) {
+    public static boolean validaNome(String nome) {
+        if (nome.matches("^[A-Za-zÀ-ÿ \"'-]+$") && nome.length() > 1) {
             System.out.println("Nome registrado com sucesso!");
             return true;
         }
@@ -22,12 +22,12 @@ public class Validador {
 
     /**
      * Valida via regex se a String recebida contem apenas numeros, nao aceitando letras e caracteres especiais.
-     * @param s parametro de entrada enviado pelo usuario.
+     * @param telefone parametro de entrada enviado pelo usuario.
      * @return Este metodo retorna verdadeira se tiver apenas numeros com tamanho em 10 e 11 digitos;
      *                     retorna falso se receber algo que não seja numeros.
      */
-    public static boolean validaTelefone(String s) {
-        if (s.matches("\\d+") && s.length() > 9 && s.length() < 12) {
+    public static boolean validaTelefone(String telefone) {
+        if (telefone.matches("\\d+") && telefone.length() > 9 && telefone.length() < 12) {
             System.out.println("Numero de telefone registrado com sucesso!");
             return true;
         }
@@ -42,12 +42,12 @@ public class Validador {
      *      Ano = entre 0000 e 9999;
      *      Separacao = obrigatoriamente / .
      *      (nao verifica ano bisexto nem limita dia em funcao do mes)
-     * @param s parametro de entrada enviado pelo usuario.
+     * @param data parametro de entrada enviado pelo usuario.
      * @return Este metodo retorna verdadeira se receber dados no formato dd/MM/yyyy;
      *                     retorna falso se nao satisfazer a condicao.
      */
-    public static boolean validaData(String s) {
-        if (s.matches("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$")) {
+    public static boolean validaData(String data) {
+        if (data.matches("^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$")) {
             System.out.println("Data de Nascimento registrada com sucesso!");
             return true;
         }
@@ -57,12 +57,12 @@ public class Validador {
 
     /**
      * Valida via regex se a String recebida contem apenas numeros, com no maximo uma casa decimal, separada por ".".
-     * @param s parametro de entrada enviado pelo usuario.
+     * @param nota parametro de entrada enviado pelo usuario.
      * @return Este metodo retorna verdadeira se tiver apenas numeros entre 0 e 10;
      *                     retorna falso se receber algo que nao seja numeros.
      */
-    public static boolean validaNota(String s) {
-        if (s.matches("^(10)$|(10.0)$|^[0-9](\\.[0-9])?$")) {
+    public static boolean validaNota(String nota) {
+        if (nota.matches("^(10)$|(10.0)$|^[0-9](\\.[0-9])?$")) {
             System.out.println("Nota registrada com sucesso!");
             return true;
         }
@@ -72,12 +72,12 @@ public class Validador {
 
     /**
      * Valida via regex se a String recebida contem apenas numeros inteiros.
-     * @param s parametro de entrada enviado pelo usuario.
+     * @param id parametro de entrada enviado pelo usuario.
      * @return Este metodo retorna verdadeira se tiver apenas numeros;
      *                     retorna falso se receber algo que nao seja numeros.
      */
-    public static boolean validaID(String s) {
-        if (s.matches("^[0-9]*$") && !s.equals("")) {
+    public static boolean validaID(String id) {
+        if (id.matches("^[0-9]*$") && !id.equals("")) {
             return true;
         }
         System.out.println("Por favor, insira apenas numeros.");
